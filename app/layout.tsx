d93b6e13fd94b00be779048874ef7134e499b4b1
jisunyang0808 <75171081+jisunyang0808@users.noreply.github.com>
2026-02-25 23:36:40 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
+import React from "react";
+import "./globals.css"; // 이 줄이 에러가 난다면 삭제해도 됩니다.
+
 export const metadata = {
-  title: '아이세이프 (BabySafe)',
-  description: '우리아이 안전을 위한 리콜 정보 서비스',
-}
+  title: "베이비 세이프",
+  description: "아이 안전 리콜 정보",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  )
+  );
 }
